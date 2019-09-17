17bcd03b1176a522a98617849e3d580962e384ca
fmouly01 <fmouly01@pcalb-mm0618.univ-jfc.fr>
2019-09-17 13:39:05 +0200

    test Commit git

---
+++ src/tpgit/TPGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgit;
+
+/**
+ *
+ * @author fmouly01
+ */
+public class TPGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // test commit git 
+        System.out.println("tpgit.TPGit.main()");
+    }
+    
+}
