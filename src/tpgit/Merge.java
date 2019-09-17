66482c12d6746a90efd3cf2cf71ac1d5bc74ac79
fmouly01 <fmouly01@pcalb-mm0618.univ-jfc.fr>
2019-09-17 14:37:11 +0200

    test Commit git

---
+++ src/tpgit/Merge.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgit;
+
+/**
+ *
+ * @author fmouly01
+ */
+public class Merge {
+    public static void main (String[] args){
+        System.out.println("test");
+    }
+}
